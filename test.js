03080c3ce4c1e1537f38e22224bfc6670dccba86
박효상 <45405912+pd0203@users.noreply.github.com>
2021-12-09 10:48:54 +0900

    first commit

---
+++ test.js
@@ -0,0 +1,12 @@
+function getEven() {
+   let arr = [];
+   
+   for (let i = 1; i <= 50; i++) {
+        if (i%2 === 0) {
+            arr.push(i);
+		}
+   } 
+   return arr;
+}
+
+console.log(getEven());
